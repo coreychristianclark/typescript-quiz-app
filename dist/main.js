45fbc3be84f8cacac065eb607b4a0514ec5c1d13
Corey Clark <coreychristianclark@gmail.com>
2024-02-13 13:22:26 -0500

    The first question is recognizing the answer successfully. Just have to 'pretty up' the selection and submission process

--- dist/main.js
+++ dist/main.js
@@ -1,58 +1,56 @@
 document.addEventListener("DOMContentLoaded", function (e) {
+    var question1Container = document.querySelector("#question1Container");
     var choice = document.querySelectorAll(".choice");
     var choice2 = document.querySelectorAll(".choice2");
     var choice3 = document.querySelectorAll(".choice3");
     var bonus = document.querySelector("#bonus");
     var submit = document.querySelector("#submit");
-    var correctAnswers = {
-        question1: "V8",
-        question2: "Armalite Rifle",
-        question3: "Coat",
-        questionBonus: "16",
-    };
-    var isCorrect = function (questionNum, selection) {
-        return correctAnswers[questionNum] === selection;
-    };
-    choice.forEach(function (option) {
-        option.addEventListener("click", function (e) {
-            var selectedChoice = option.innerText.trim();
-            if (isCorrect("question1", selectedChoice)) {
-                console.log("Nice!");
-            }
-            else {
-                console.log("not nice");
-            }
-        });
-    });
-    choice2.forEach(function (option) {
-        option.addEventListener("click", function (e) {
-            var selectedChoice = option.innerText.trim();
-            if (isCorrect("question2", selectedChoice)) {
-                console.log("extra nice");
-            }
-            else {
-                console.log("extra not nice");
-            }
-        });
-    });
-    choice3.forEach(function (option) {
-        option.addEventListener("click", function (e) {
-            var selectedChoice = option.innerText.trim();
-            if (isCorrect("question3", selectedChoice)) {
-                console.log("ultra nice");
-            }
-            else {
-                console.log("ultra not nice");
-            }
-        });
-    });
-    submit.addEventListener("click", function (e) {
-        var userInput = bonus.value.trim();
-        if (isCorrect("questionBonus", userInput)) {
-            console.log("Outstanding!");
+    var clickedElement;
+    var selectedChoice;
+    var correctAnswer;
+    // const userInput = clickedElement.value.trim();
+    // const questions: string[] = ["question1", "question2", "question3", "questionBonus"];
+    // const isCorrect = function (question: string, selection: string): boolean {
+    //   return questions[question] === selection;
+    // };
+    question1Container.addEventListener("toggle", function (e) {
+        clickedElement = e.target;
+        selectedChoice = clickedElement.innerText.trim();
+        correctAnswer = "No";
+        clickedElement.classList.add(".bold");
+        if (selectedChoice === correctAnswer) {
+            console.log("Nice!");
+            clickedElement.classList.add(".correct");
         }
         else {
-            console.log("Not quite.");
+            console.log("not nice");
         }
     });
+    // choice2.forEach(function (option) {
+    //   option.addEventListener("click", (e) => {
+    //     const selectedChoice = option.textContent.trim();
+    //     if (isCorrect("question2", selectedChoice)) {
+    //       console.log("extra nice");
+    //     } else {
+    //       console.log("extra not nice");
+    //     }
+    //   });
+    // });
+    // choice3.forEach(function (option) {
+    //   option.addEventListener("click", (e) => {
+    //     const selectedChoice = option.textContent.trim();
+    //     if (isCorrect("question3", selectedChoice)) {
+    //       console.log("ultra nice");
+    //     } else {
+    //       console.log("ultra not nice");
+    //     }
+    //   });
+    // });
+    // submit.addEventListener("click", (e) => {
+    //   if (isCorrect("questionBonus", userInput)) {
+    //     console.log("Outstanding!");
+    //   } else {
+    //     console.log("Not quite.");
+    //   }
+    // });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,74 @@
 document.addEventListener("DOMContentLoaded", (e) => {
+
+  const question1Container = document.querySelector("#question1Container")
   const choice = document.querySelectorAll(".choice");
   const choice2 = document.querySelectorAll(".choice2");
   const choice3 = document.querySelectorAll(".choice3");
+
   const bonus = document.querySelector("#bonus");
   const submit = document.querySelector("#submit");
 
-  const correctAnswers = {
-    question1: "V8",
-    question2: "Armalite Rifle",
-    question3: "Coat",
-    questionBonus: "16",
-  };
+  let clickedElement: HTMLElement;
+  let selectedChoice: string;
+  let correctAnswer: string | boolean;
+  // const userInput = clickedElement.value.trim();
 
-  const isCorrect = function (questionNum: string, selection: string) {
-    return correctAnswers[questionNum] === selection;
-  };
 
-  choice.forEach(function (option) {
-    option.addEventListener("click", (e) => {
-      const selectedChoice = option.innerText.trim();
+  // const questions: string[] = ["question1", "question2", "question3", "questionBonus"];
 
-      if (isCorrect("question1", selectedChoice)) {
-          console.log("Nice!")
-      } else {
-        console.log("not nice");
-      }
-    });
-  });
+  // const isCorrect = function (question: string, selection: string): boolean {
+  //   return questions[question] === selection;
+  // };
 
-  choice2.forEach(function (option) {
-    option.addEventListener("click", (e) => {
-      const selectedChoice = option.innerText.trim();
 
-      if (isCorrect("question2", selectedChoice)) {
-        console.log("extra nice");
-      } else {
-        console.log("extra not nice");
-      }
-    });
-  });
+  question1Container.addEventListener("toggle", (e) => {
 
-  choice3.forEach(function (option) {
-    option.addEventListener("click", (e) => {
-      const selectedChoice = option.innerText.trim();
+    clickedElement = e.target as HTMLElement;
+    selectedChoice = clickedElement.innerText.trim();
+    correctAnswer = "No";
 
-      if (isCorrect("question3", selectedChoice)) {
-        console.log("ultra nice");
-      } else {
-        console.log("ultra not nice");
-      }
-    });
-  });
+    clickedElement.classList.add(".bold");
 
-  submit.addEventListener("click", (e) => {
+    if (selectedChoice === correctAnswer) {
+      console.log("Nice!")
+      clickedElement.classList.add(".correct")
 
-    const userInput = bonus.value.trim();
-
-    if (isCorrect("questionBonus", userInput)) {
-      console.log("Outstanding!");
     } else {
-      console.log("Not quite.");
+      console.log("not nice");
     }
   });
+
+  // choice2.forEach(function (option) {
+  //   option.addEventListener("click", (e) => {
+  //     const selectedChoice = option.textContent.trim();
+
+  //     if (isCorrect("question2", selectedChoice)) {
+  //       console.log("extra nice");
+  //     } else {
+  //       console.log("extra not nice");
+  //     }
+  //   });
+  // });
+
+  // choice3.forEach(function (option) {
+  //   option.addEventListener("click", (e) => {
+  //     const selectedChoice = option.textContent.trim();
+
+  //     if (isCorrect("question3", selectedChoice)) {
+  //       console.log("ultra nice");
+  //     } else {
+  //       console.log("ultra not nice");
+  //     }
+  //   });
+  // });
+
+  // submit.addEventListener("click", (e) => {
+
+
+  //   if (isCorrect("questionBonus", userInput)) {
+  //     console.log("Outstanding!");
+  //   } else {
+  //     console.log("Not quite.");
+  //   }
+  // });
 });
